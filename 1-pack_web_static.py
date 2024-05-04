c2b15d7fb56b104be96d176472ac5b78d870f6df
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-05-04 03:07:25 +0100

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""A module for Fabric script that generates a .tgz archive."""
+"""this module for script that generates that archive."""
 import os
 from datetime import datetime
 from fabric.api import local, runs_once
@@ -7,7 +7,7 @@ from fabric.api import local, runs_once
 
 @runs_once
 def do_pack():
-    """Archives the static files."""
+    """this will Archives the static files."""
     if not os.path.isdir("versions"):
         os.mkdir("versions")
     d_time = datetime.now()
