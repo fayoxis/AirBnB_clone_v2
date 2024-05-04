0c66ab8a4d88dd42b5dd0a5092a53259b7771482
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-05-04 03:25:48 +0100

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Fabric script to genereate tgz archive
-execute: fab -f 1-pack_web_static.py do_pack
+this script to genereate to archive
+execute: fab -f 1-pack_web_static.py
 """
 
 from datetime import datetime
@@ -10,7 +10,7 @@ from fabric.api import *
 
 def do_pack():
     """
-    making an archive on web_static folder
+    this will make an archive on web_static folder
     """
 
     time = datetime.now()
