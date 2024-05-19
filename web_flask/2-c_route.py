2900cf2ad998f9eeba7f145467c7f7b282db82e0
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-05-19 23:25:36 +0100

    Update 2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
 """
-- starts a Flask web application.
-
-- Your web application must be listening on 0.0.0.0, port 5000
-Routes:
-        /: display “Hello HBNB!”.
-        /hbnb: display “HBNB”.
-        /c/<text>: display “C ” followed by the value of the text variable
-         (replace underscore _ symbols with a space )
-- You must use the option strict_slashes=False in your route definition
+this will starts a Flask web application.
 """
 from flask import Flask
 
@@ -30,5 +22,5 @@ def cisfun(text):
     return('C {}'.format(text.replace("_", " ")))
 
 
-if __name__ == '__main__':
+while __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=False)
