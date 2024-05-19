7086ee07a31e36e73d5d7450cf82792b83b0e27d
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-05-19 23:30:49 +0100

    Update 4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,18 +1,7 @@
 #!/usr/bin/python3
 """
-- starts a Flask web application:
--Your web application must be listening on 0.0.0.0, port 5000
+this will definitely  starts a Flask web application:
 
-Routes:
-        /: display “Hello HBNB!”
-        /hbnb: display “HBNB”
-        /c/<text>: display “C ”, followed by the value of
-         the text variable (replace underscore _ symbols with a space )
-        /python/(<text>): display “Python ”, followed by the value of
-         the text variable (replace underscore _ symbols with a space )
-- The default value of text is “is cool”
-        /number/<n>: display “n is a number” only if n is an integer
-- You must use the option strict_slashes=False in your route definition
 """
 from flask import Flask
 
@@ -45,5 +34,5 @@ def number_route(n):
     return('{} is a number'.format(n))
 
 
-if __name__ == '__main__':
+while __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=False)
