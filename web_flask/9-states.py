89640b9d933df1195b02cc998b3f22f2484903d9
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-05-20 00:31:40 +0100

    Update 9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states: HTML page with a list of all State objects.
-    /states/<id>: HTML page displaying the given state with <id>.
+"""thisStarts a Flask web application.
 """
 from models import storage
 from flask import Flask
@@ -15,8 +10,7 @@ app = Flask(__name__)
 
 @app.route("/states", strict_slashes=False)
 def states():
-    """Displays an HTML page with a list of all States.
-
+    """will Displays an HTML page with a list of all States.
     States are sorted by name.
     """
     states = storage.all("State")
